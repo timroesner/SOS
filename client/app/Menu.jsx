4407d524286d2b87f3606289b33b3e7214ab081e
LittleBoiTien <Tien.Nguyen072399@gmail.com>
2018-09-09 11:49:44 -0700

    updated header

--- client/app/Menu.jsx
+++ client/app/Menu.jsx
@@ -8,22 +8,43 @@ export default class Header extends React.Component {
 		this.state = {menuOpen:false}
 	}
 
-	handleClick() {
+	handleClick(event) {
 		this.setState({menuOpen: !this.state.menuOpen})
+		console.log("click")
 	}
 
     render () {
         return (
-        <div style = {{height:"100%", width:"100%"}}>
-        {this.state.menuOpen && <div style = {{height:"100%", width:"100%", opacity:"95%", color:"lightGray"}}> </div>}
+
+
+        	
+
+
         	<header style = {{backgroundColor: "#ff4d4d", height: "60px", margin: "-18px -28px 0 -28px"}}>
-        		<h1 style = {{color:"white", paddingTop: "15px"}}>SOS</h1>
+
+        		<h1 style = {{color:"white", paddingTop: "12px"}}>SOS</h1>
         		
-        		<nav style = {{margin: "-60px 0 0 20px"}}>
-        			<p style = {{color:"white", fontSize: "25px"}} onClick = {() => this.handleClick()}>&#9776;</p>
-        		</nav>
+        		<nav style = {{margin: "-61px 0 0 20px", width:"21px", zIndex:"100"}}>
+        			<button style = {{color:"white", fontSize: "25px", marginTop: "13px"}} onClick = {() => this.handleClick(this.event)}>&#9776;</button>
+        			
+        			{
+        			 this.state.menuOpen && 
+			     		 (
+			          <div style = {{backgroundColor:"white", shadowOpacity: "95%", margin:"9px 0 0 -28px", width:"383px", height:"607px", position:"absolute"}}>
+			          	<hr/><p style = {{color: "#ff4d4d", fontSize:"24px", margin:"5px 40px 0 40px", paddingTop:"10px"}}>Surivors</p>
+			          	<hr/><p style = {{color: "#ff4d4d", fontSize:"24px", margin:"5px 40px 0 40px", paddingTop:"10px"}}>First Responders</p>
+			          	<hr/><p style = {{color: "#ff4d4d", fontSize:"24px", margin:"5px 40px 0 40px", paddingTop:"10px"}}>Chat</p>
+			          	<hr/>
+
+			            {/*<button style = {{color:"blue", fontSize:"14px", marginTop:"10px", backgroundColor:"white"}}> Menu item 1 </button> <br/>
+			            <button style = {{color:"blue", fontSize:"14px", marginTop:"10px", backgroundColor:"white"}}> Menu item 2 </button><br/>
+			            <button style = {{color:"blue", fontSize:"14px", marginTop:"10px", backgroundColor:"white"}}> Menu item 3 </button><br/>*/}
+			          </div>
+			        )
+
+       				 }
+        		</nav> 
         	</header>
-        </div>
        )
    }
 }
